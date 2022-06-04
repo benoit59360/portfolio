84120eba8b640e6b7726aba89239b435189e3be1
Ben ben <71250999+benoit59360@users.noreply.github.com>
2022-06-04 10:00:08 +0200

    Ajout initial des fichiers WEB

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Je suis ______, je suis actuellement en Bachelor - Chef de Projet Digital et vous pourrez y trouver les projets que j'ai pu effectuer durant mon année."/>
+
+	<title>BRASSELET Benoit - Portfolio</title>
+
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+	<header>
+		<div class="rect">
+			
+		</div>
+		<!--<img src="assets/img/forme_droite.png">-->
+	</header>
+
+	<div class="hero">
+		<img src="assets/img/forme_droite.png">
+	</div>
+
+	<div class="about-me">
+		
+	</div>
+
+</body>
+</html>
